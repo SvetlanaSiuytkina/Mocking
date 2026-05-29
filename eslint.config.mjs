ccfbfa94532e2552387967faa7eb9874ae33ba4c
SVETA <svetadan1234@mail.ru>
2026-05-29 22:37:27 +0500

    Ignore coverage, fix, added rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import jest from "eslint-plugin-jest";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import jest from 'eslint-plugin-jest';
 
 export default defineConfig([
   js.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -16,17 +16,20 @@ export default defineConfig([
     },
     plugins: {jest},
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-      "no-console": "warn",
-      "eqeqeq": "error",
-      "no-unused-vars": "error",
-      "no-var": "error",
-      "prefer-const": "error",
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-console': 'warn',
+      'eqeqeq': 'error',
+      'no-unused-vars': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'eol-last': ['error', 'always'],
+      'indent': ['error', 2],
+      'space-infix-ops': 'error',
       
-      "jest/no-disabled-tests": "warn",
-      "jest/no-focused-tests": "error",
-      "jest/no-identical-title": "error"
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error'
     },
   },
 ]);
--- src/tests/getLevel.test.js
+++ src/tests/getLevel.test.js
@@ -4,26 +4,36 @@ import fetchData from '../http.js';
 jest.mock('../http.js');
 
 beforeEach(() => {
-    jest.resetAllMocks();
+  jest.resetAllMocks();
+});
+
+test('should return an error message if fetchData throws an error', () => {
+  fetchData.mockImplementation(() => {
+    throw new Error('Network error');
+  });
+    
+  const result = getLevel(1);
+  expect(result).toBe('Информация об уровне временно недоступна');
+  expect(fetchData).toHaveBeenCalledWith(1);
 });
 
 test('should return the user level if the status is ok', () => {
-    fetchData.mockReturnValue({
-        status: 'ok',
-        level: 1
-    });
+  fetchData.mockReturnValue({
+    status: 'ok',
+    level: 1
+  });
 
-    const result = getLevel(5);
-    expect(result).toBe('Ваш текущий уровень: 1');
-    expect(fetchData).toHaveBeenCalledWith(5);
+  const result = getLevel(5);
+  expect(result).toBe('Ваш текущий уровень: 1');
+  expect(fetchData).toHaveBeenCalledWith(5);
 });
 
 test('should return an error message if the status is not ok', () => {
-    fetchData.mockReturnValue({
-        status: 'error'
-    });
+  fetchData.mockReturnValue({
+    status: 'error'
+  });
 
-    const result = getLevel(500);
-    expect(result).toBe('Информация об уровне временно недоступна');
-    expect(fetchData).toHaveBeenCalledWith(500);
+  const result = getLevel(500);
+  expect(result).toBe('Информация об уровне временно недоступна');
+  expect(fetchData).toHaveBeenCalledWith(500);
 });
